27149e08b760ef1597d145456a6b53ab70491849
Asmita Dahule <dahuleasmita@gmail.com>
2025-10-15 06:16:13 +0530

    props in react js

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -5,9 +5,9 @@ import Profile from './components/Profile'
 const App = () => {
   return (
     <>
-      <Profile />
-      <Profile />
-      <Profile />
+      <Profile name="Asmi Dahu" age={21}/>
+      <Profile name="Amisha Daf" age={20}/>
+      <Profile name="Tina Dabi" age={22}/>
     </>
   )
 }
--- 01-folder/src/components/Profile.jsx
+++ 01-folder/src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Profile = () => {
+const Profile = (user) => {
   return (
     <div className='parent'>
       <div className="card">
         <img src="https://tse2.mm.bing.net/th/id/OIP.B39-1EvwOFXOffOfIKZT0AHaEK?pid=Api&P=0&h=180" alt="" />
-        <h1>Asmita Dahule</h1>
+        <h1>{user.name}, {user.age}</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
         <button>View Profile</button>
       </div>
