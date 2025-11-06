9270f179a6888ca3ecc131cc8e889a747727fee2
Asmita Dahule <dahuleasmita@gmail.com>
2025-11-06 22:11:42 +0530

    props learning

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -4,14 +4,14 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card name='Asmita' age={21} img="https://imgs.search.brave.com/9fXjViqCCgggq6LImtxhHgpjhTEdZJet8zBZAeXGSVQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ5/ODI1Njk1NS9waG90/by9zdHJlZXQtcG9y/dHJhaXQtb2YtYS13/b21hbi1vbi1hLWdy/ZWVuLWJhY2tncm91/bmQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWZ5b0JrMjlz/bnJBLU1ScExjdFVY/bXJlR2J1NDBYSlBY/Y0RvSXFZdzdfZnc9"/>
+    <Card name='sdfd' age={25} img="https://imgs.search.brave.com/9fXjViqCCgggq6LImtxhHgpjhTEdZJet8zBZAeXGSVQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ5/ODI1Njk1NS9waG90/by9zdHJlZXQtcG9y/dHJhaXQtb2YtYS13/b21hbi1vbi1hLWdy/ZWVuLWJhY2tncm91/bmQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWZ5b0JrMjlz/bnJBLU1ScExjdFVY/bXJlR2J1NDBYSlBY/Y0RvSXFZdzdfZnc9"/>
+    <Card name='uytre' age={81} img="https://imgs.search.brave.com/HSB0f_dLkBNxu_rYYjHv3ru1ajZ1_8802P8HO9m-dUA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTM4/NzA5MzYyL3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtZ2lybC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9VVFH/WHBlaUxySTc4bk8x/QjlwZVVuMEQwZkNT/UnJtLUo4eG9oTVdH/Mkxtcz0"/>
+    <Card name='pikjh' age={41} img="https://imgs.search.brave.com/9fXjViqCCgggq6LImtxhHgpjhTEdZJet8zBZAeXGSVQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ5/ODI1Njk1NS9waG90/by9zdHJlZXQtcG9y/dHJhaXQtb2YtYS13/b21hbi1vbi1hLWdy/ZWVuLWJhY2tncm91/bmQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWZ5b0JrMjlz/bnJBLU1ScExjdFVY/bXJlR2J1NDBYSlBY/Y0RvSXFZdzdfZnc9"/>
+    <Card name='asf' age={21} img="https://imgs.search.brave.com/HSB0f_dLkBNxu_rYYjHv3ru1ajZ1_8802P8HO9m-dUA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTM4/NzA5MzYyL3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtZ2lybC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9VVFH/WHBlaUxySTc4bk8x/QjlwZVVuMEQwZkNT/UnJtLUo4eG9oTVdH/Mkxtcz0"/>
+    <Card name='tertr' age={37} img="https://imgs.search.brave.com/9fXjViqCCgggq6LImtxhHgpjhTEdZJet8zBZAeXGSVQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ5/ODI1Njk1NS9waG90/by9zdHJlZXQtcG9y/dHJhaXQtb2YtYS13/b21hbi1vbi1hLWdy/ZWVuLWJhY2tncm91/bmQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWZ5b0JrMjlz/bnJBLU1ScExjdFVY/bXJlR2J1NDBYSlBY/Y0RvSXFZdzdfZnc9"/>
+    <Card name='kkhyt' age={58} img="https://imgs.search.brave.com/HSB0f_dLkBNxu_rYYjHv3ru1ajZ1_8802P8HO9m-dUA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTM4/NzA5MzYyL3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtZ2lybC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9VVFH/WHBlaUxySTc4bk8x/QjlwZVVuMEQwZkNT/UnJtLUo4eG9oTVdH/Mkxtcz0"/>
+    <Card name='fsdf' age={12} img="https://imgs.search.brave.com/9fXjViqCCgggq6LImtxhHgpjhTEdZJet8zBZAeXGSVQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ5/ODI1Njk1NS9waG90/by9zdHJlZXQtcG9y/dHJhaXQtb2YtYS13/b21hbi1vbi1hLWdy/ZWVuLWJhY2tncm91/bmQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWZ5b0JrMjlz/bnJBLU1ScExjdFVY/bXJlR2J1NDBYSlBY/Y0RvSXFZdzdfZnc9"/>
     </div>
   )
 }
--- react-basics/src/components/Card.jsx
+++ react-basics/src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
       <div className='card'>
-        <img src="https://imgs.search.brave.com/HSB0f_dLkBNxu_rYYjHv3ru1ajZ1_8802P8HO9m-dUA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTM4/NzA5MzYyL3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtZ2lybC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9VVFH/WHBlaUxySTc4bk8x/QjlwZVVuMEQwZkNT/UnJtLUo4eG9oTVdH/Mkxtcz0" alt="" />
-        <h2 className='name'>Asmi Dahule</h2>
+        <img src={props.img} alt="" />
+        <h2 className='name'>{props.name}, {props.age}</h2>
         <p className='description'>Lorem ipsum dolor sit amet consectetur adipi.</p>
         <button>View Profile</button>
       </div>
