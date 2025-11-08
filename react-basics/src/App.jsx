24f5f3018fb7ba6d506bb61d2f9fd6fb968d45c0
Asmita Dahule <dahuleasmita@gmail.com>
2025-11-08 16:02:51 +0530

    mini project using useState

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectUi from './components/project-ui/ProjectUi.jsx'
 
 
 const App = () => {
+
+   const [count, setCount] = useState(0);
+
+   function incNum(){
+    setCount(count + 1);
+   }
+
+   function decNum(){
+    setCount(count - 1);
+   }
+    
   return (
-    <div  className="flex justify-center items-center h-screen bg-gray-100">
-      <ProjectUi />
+    
+    <div>
+      <div className='cnt'>{count}</div>
+      <button onClick={incNum}>increase</button>
+      <button onClick={decNum}>decrease</button>
     </div>
   )
 }
-
-export default App
+export default App 
